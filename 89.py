9134c7f3b0fb34f689b81ba82a81e5e92bc56c93
Akshit0075 <73303310+Akshit0075@users.noreply.github.com>
2024-11-11 11:02:43 +0530

    Create 89.py

---
+++ 89.py
@@ -0,0 +1,13 @@
+class car:
+  def __init__(self,method,model):
+    self.make= make
+    self.model= model
+def display_info(self):
+  return f "{self.make}{self.model}"
+
+car1 = car("Toyota","Camry")
+car2 = car("Honda","Accord")
+
+print("Car1:", car1.display_info())
+print("Car2:", car2.display_info())
+print("THIS PROGRAM IS WRITTEN BY Akshit Trikha erp:- 0221BCA008")
